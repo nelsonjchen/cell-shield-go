c9d37f699e0ccfee7159b9d7a47e55c8dc4baff8
Nelson Chen <crazysim@gmail.com>
2021-07-17 23:20:40 -0700

    Rearrange structure

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module cell-shield-go
 go 1.16
 
 require (
-	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
 	google.golang.org/api v0.50.0
 )
--- main.go
+++ main.go
@@ -1,51 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
-	"io/ioutil"
+	"cell-shield-go/shieldinformation"
 	"log"
 )
 
 func main() {
-	ctx := context.Background()
-	b, err := ioutil.ReadFile("key.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
-	//config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
+	shieldInformation, err := shieldinformation.GrabShieldInformation("", "1-m3sfTXGwoqsPrZcyJQnJ2FoClwpl67EyPwUtOmwtxo", "B3")
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		log.Fatalln(err)
 	}
-	client := config.Client(context.Background())
-
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
-
-	spreadsheetId := "1-m3sfTXGwoqsPrZcyJQnJ2FoClwpl67EyPwUtOmwtxo"
-	titleRange := "Typical"
-	resp, err := srv.Spreadsheets.Get(spreadsheetId).Ranges(titleRange).IncludeGridData(true).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
-	if len(resp.Sheets) > 0 {
-		cellValue := resp.Sheets[0].Data[0].RowData[0].Values[0]
-		formattedValue := cellValue.FormattedValue
-		backgroundColor := cellValue.EffectiveFormat.BackgroundColor
-		fmt.Println("cellValue", formattedValue)
-		fmt.Printf("%02x ", int(backgroundColor.Red*255))
-		fmt.Printf("%02x ", int(backgroundColor.Green*255))
-		fmt.Printf("%02x ", int(backgroundColor.Blue*255))
-	} else {
-		fmt.Println("No data found.", resp)
-	}
+	log.Println(*shieldInformation)
 
 }
