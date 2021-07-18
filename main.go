7d5a45f22a6899ed82b371d5990341c6dd82554d
Nelson Chen <crazysim@gmail.com>
2021-07-17 18:14:01 -0700

    Constrain result

--- main.go
+++ main.go
@@ -30,22 +30,13 @@ func main() {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
-	// Prints the names and majors of students in a sample spreadsheet:
-	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-	readRange := "Class Data!A2:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	spreadsheetId := "1-m3sfTXGwoqsPrZcyJQnJ2FoClwpl67EyPwUtOmwtxo"
+	titleRange := "Sheet1!B1"
+	resp, err := srv.Spreadsheets.Get(spreadsheetId).Ranges(titleRange).IncludeGridData(true).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
-	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
-	} else {
-		fmt.Println("Name, Major:")
-		for _, row := range resp.Values {
-			// Print columns A and E, which correspond to indices 0 and 4.
-			fmt.Printf("%s, %s\n", row[0], row[4])
-		}
-	}
+	fmt.Println("No data found.", resp)
+
 }
